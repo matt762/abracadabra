a69d0fd9962db51d7b64ea94e319a351b4c8060d
Matteo <matteo.fiore@epfl.ch>
2024-12-01 16:32:27 +0100

    Jekyll website

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var data = [{
-        x: [1, 2, 3, 4, 5],
-        y: [1, 2, 4, 8, 16],
-        type: 'scatter'
-    }];
-
-    Plotly.newPlot('plot', data);
-});
